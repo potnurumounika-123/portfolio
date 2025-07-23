0b0639ed73afff9f738e1fdd96a7db28b5249b4e
Mounika Potnuru <mounikapotnuru2405@gmail.com>
2025-07-23 21:26:58 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -42,7 +42,7 @@ const Skills = () => {
         { name: 'Wireshark'},
         { name: 'KaliLinux'},
         { name: 'Burpsuite'},
-        { name: 'Metasploi'},
+        { name: 'Metasploit'},
       ],
     },
   ];
